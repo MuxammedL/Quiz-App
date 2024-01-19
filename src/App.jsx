caf92c2d32500f7a05ef8ff9fd9f13d0a719131e
MuxammedL <layicovmuxammed@gmail.com>
2024-01-19 20:15:28 +0400

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,46 +5,46 @@ import Quiz from "./Quiz";
 function App() {
   const [connection, setConnection] = useState();
 
-  const joinGame = async () => {
-    try {
-      const newConnection = new HubConnectionBuilder()
-        .withUrl(
-          "https://mindmazeprojectwebapi-6nortrmkbq-ey.a.run.app/playhub"
-        )
-        .configureLogging(LogLevel.Information)
-        .build();
+  // const joinGame = async () => {
+  //   try {
+  //     const newConnection = new HubConnectionBuilder()
+  //       .withUrl(
+  //         ""
+  //       )
+  //       .configureLogging(LogLevel.Information)
+  //       .build();
 
-      newConnection.on("GetOnlineUsers", (onlineUsers) => {
-        console.log("Received online users: ", onlineUsers);
-      });
+  //     newConnection.on("GetOnlineUsers", (onlineUsers) => {
+  //       console.log("Received online users: ", onlineUsers);
+  //     });
 
-      newConnection.on("RecMessage", (str1, str2) => {
-        console.log(str1, str2);
-      });
+  //     newConnection.on("RecMessage", (str1, str2) => {
+  //       console.log(str1, str2);
+  //     });
 
-      setConnection(newConnection); // Set the connection state
+  //     setConnection(newConnection); // Set the connection state
 
-      await newConnection.start();
-      // Invoke BeOnline method and handle any response
+  //     await newConnection.start();
+  //     // Invoke BeOnline method and handle any response
 
-      await newConnection.invoke("BeOnline", {
-        idToken: 'LM59',
-        userName: "MuxammedZi",
-        point: 1502,
-      });
+  //     await newConnection.invoke("BeOnline", {
+  //       idToken: 'LM59',
+  //       userName: "MuxammedZi",
+  //       point: 1502,
+  //     });
 
-      // Invoke GetOnlineUsers and handle the response
-      // const onlineUsers = await newConnection.invoke("GetOnlineUsers");
-      // const onlineFriends = await newConnection.invoke(
-      //   "GetOnlineFriends",
-      //   token
-      // );
-      // console.log("Online users:", onlineUsers);
-      // console.log("Online friends:", onlineFriends);
-    } catch (error) {
-      console.error("Error joining game:", error);
-    }
-  };
+  //     // Invoke GetOnlineUsers and handle the response
+  //     // const onlineUsers = await newConnection.invoke("GetOnlineUsers");
+  //     // const onlineFriends = await newConnection.invoke(
+  //     //   "GetOnlineFriends",
+  //     //   token
+  //     // );
+  //     // console.log("Online users:", onlineUsers);
+  //     // console.log("Online friends:", onlineFriends);
+  //   } catch (error) {
+  //     console.error("Error joining game:", error);
+  //   }
+  // };
 
   const [questions, setQuestions] = useState(null);
 
@@ -56,7 +56,7 @@ function App() {
 
   useEffect(() => {
     getQuestions();
-    joinGame()
+
   }, []);
   console.log(questions);
   return <>{questions && <Quiz questions={questions} />}</>;
